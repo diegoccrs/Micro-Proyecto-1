b57f6040aa2092ed0e5fde71f8a33d61a026e3b7
Andres Rangel <andres.rangel@correo.unimet.edu.ve>
2024-05-25 11:29:05 -0400

    arreglado estadsiticas

--- js/script.js
+++ js/script.js
@@ -120,21 +120,22 @@ document.addEventListener('keydown', function(event){
           document.getElementById("input-text").style.display = "block";
           document.getElementById("btn-iniciar").textContent = "INICIAR JUEGO";
           document.getElementById("btn-reiniciar").textContent = "REINICIAR";
+          //añadir a la tabla
+          //no cambiar el orden de las siguientes lineas
+          appendRow();
           puntuacionNum = 0;
           rachaNum = 0;
 
-          finalJuego = true;
-
-          //añadir a la tabla
-          appendRow();
+          
           intentoNum++;
+          finalJuego = true;
       }
 
       if (ganador(palabraRandom,arrayLetrasCorrectas) && !finalJuego){
             drawFinJuego("Ganaste, Felicidades");
             document.getElementById("input-text").style.display = "block";
             finalJuego = true;
-
+            iniciarJuego.style.display = "block";
             //racha y puntaje
             definirPuntaje();
             }
@@ -201,9 +202,9 @@ iniciarJuego.addEventListener("click", function (evt){
   arrayLetrasCorrectas = [];
   arrayLetrasIncorrectas =[];
   finalJuego = false;
-
-  document.getElementById("input-text").style.display = "none";
   iniciarJuego.textContent = "CONTINUAR";
+  iniciarJuego.style.display = "none";
+  document.getElementById("input-text").style.display = "none";
   palabraRandom = listaPalabras(basePalabras);
   console.log(palabraRandom);
 
@@ -267,6 +268,7 @@ btnReiniciar.addEventListener("click", function() {
     let btnReiniciar = document.getElementById("btn-reiniciar");
     var racha = document.querySelector("#racha");
     var puntuacion = document.querySelector("#puntuacion");
+    iniciarJuego.style.display = "block";
     iniciarJuego.textContent = "INICIAR JUEGO";
 
     // Coloca el texto en el boton "btn-reiniciar" 
